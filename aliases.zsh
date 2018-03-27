79d5aa9f3df0bb0b4c3b1d7a9525d51237280ff8
Andrew McWilliams <andy@jahya.net>
2018-03-27 15:12:46 -0400

    Adds aliases for backup

--- aliases.zsh
+++ aliases.zsh
@@ -11,6 +11,12 @@ alias z='~/.oh-my-zsh'
 alias zc='~/.oh-my-zsh/custom'
 alias zca='vi ~/.oh-my-zsh/custom/aliases.zsh'
 
+alias bu="buw && bul && bum && bug"
+alias buw="rsync -avzh /Volumes/Lizard\ Man/Working/* /Volumes/Meka\ Dragon/Working/"
+alias bul="rsync -avzh /Volumes/Lizard\ Man/Libraries/* /Volumes/Meka\ Dragon/Libraries/"
+alias bum="rsync -avzh /Volumes/Lizard\ Man/Media/* /Volumes/Meka\ Dragon/Media/"
+alias bug="rsync -avzh /Volumes/Lizard\ Man/Games/* /Volumes/Meka\ Dragon/Games/"
+
 alias grs='grunt serve'
 alias grd='grunt deploy'
 alias grb='grunt build:dist'
