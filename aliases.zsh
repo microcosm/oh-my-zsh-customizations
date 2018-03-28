edb416e85e9d6dbb5eba9f6105ff398aac50ba2a
Andrew McWilliams <andy@jahya.net>
2018-03-27 20:47:36 -0400

    Updates backup folder locations

--- aliases.zsh
+++ aliases.zsh
@@ -12,10 +12,10 @@ alias zc='~/.oh-my-zsh/custom'
 alias zca='vi ~/.oh-my-zsh/custom/aliases.zsh'
 
 alias bu="buw && bul && bum && bug"
-alias buw="rsync -avzh /Volumes/Lizard\ Man/Working/* /Volumes/Meka\ Dragon/Working/"
-alias bul="rsync -avzh /Volumes/Lizard\ Man/Libraries/* /Volumes/Meka\ Dragon/Libraries/"
-alias bum="rsync -avzh /Volumes/Lizard\ Man/Media/* /Volumes/Meka\ Dragon/Media/"
-alias bug="rsync -avzh /Volumes/Lizard\ Man/Games/* /Volumes/Meka\ Dragon/Games/"
+alias buw="rsync -avzh /Volumes/Lizard\ Man/Working/* /Volumes/Meka\ Dragon/folder-backups/Working/"
+alias bul="rsync -avzh /Volumes/Lizard\ Man/Libraries/* /Volumes/Meka\ Dragon/folder-backups/Libraries/"
+alias bum="rsync -avzh /Volumes/Lizard\ Man/Media/* /Volumes/Meka\ Dragon/folder-backups/Media/"
+alias bug="rsync -avzh /Volumes/Lizard\ Man/Games/* /Volumes/Meka\ Dragon/folder-backups/Games/"
 
 alias grs='grunt serve'
 alias grd='grunt deploy'
