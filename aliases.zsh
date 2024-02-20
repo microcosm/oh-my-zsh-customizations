917c2e58ed13b11fb47dd884b96c652e69b6aa4b
Andrew McWilliams <andy@jahya.net>
2024-02-20 10:53:01 -0500

    Adds new aliases

--- aliases.zsh
+++ aliases.zsh
@@ -2,8 +2,11 @@ alias aah='~/Repos/artahack.io'
 alias ja='~/Repos/jahya.net'
 alias re='~/Repos/'
 
+alias zc="~/.oh-my-zsh/custom"
+alias sz=". ~/.zshrc"
+
 alias p8='ping 8.8.8.8'
 
 alias js='docker-compose up'
 
-alias cmd='cat ~/.oh-my-zsh/custom/aliases.zsh | grep -v "^$"'
+alias cmd='cat ~/.oh-my-zsh/custom/aliases.zsh | grep -v "^$" | sed \$d'
