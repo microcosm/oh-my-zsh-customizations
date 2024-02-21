f6136d1155d5f4660e59b4fa2f0006065d076b1a
Andrew McWilliams <andy@jahya.net>
2024-02-21 10:51:49 -0500

    Adds alias for jekyll interactivity

--- aliases.zsh
+++ aliases.zsh
@@ -7,6 +7,8 @@ alias sz=". ~/.zshrc"
 
 alias p8='ping 8.8.8.8'
 
+# https://github.com/BretFisher/jekyll-serve
 alias js='docker-compose up'
+alias jcli='docker run -v $(pwd):/site -it --entrypoint bash bretfisher/jekyll'
 
 alias cmd='cat ~/.oh-my-zsh/custom/aliases.zsh | grep -v "^$" | sed \$d'
