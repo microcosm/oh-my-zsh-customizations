07d8d8b80cdfa689b670a071d2f93930a6d2b02a
Andrew McWilliams <andy@jahya.net>
2024-03-03 21:25:38 -0500

    Adds docker aliases

--- aliases.zsh
+++ aliases.zsh
@@ -10,6 +10,8 @@ alias sz=". ~/.zshrc"
 alias p8='ping 8.8.8.8'
 
 # https://github.com/BretFisher/jekyll-serve
+alias dc='open -a Docker'
+alias dck="pkill -SIGHUP -f /Applications/Docker.app 'docker serve'"
 alias js='docker-compose up'
 alias jcli='docker run -v $(pwd):/site -it --entrypoint bash bretfisher/jekyll'
 
